f3f196e60969fa6b8aaca6142d72d6edd2ac78b7
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-10-11 14:10:18 +0900

    defined index, show, new, and create actions for RestaurantsController

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,9 +9,17 @@ def show
 end
 
 def new
+  @restaurant = Restaurant.new
 end
 
 def create
+  @restaurant = Restaurant.new(restaurant_params)
+  @restaurant.save
+  if @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  else
+    render :new
+  end
 end
 
 def edit
@@ -21,6 +29,12 @@ def update
 end
 
 def destroy
+end
+
+private
+
+def restaurant_params
+  params.require(:restaurant).permit(:name, :address, :phone_number, :category)
 end
 
 end
